6dd542092ba82dc9a73a72e9d10d75fba3c17d6c
Adjuntor <10227133+Adjuntor@users.noreply.github.com>
2026-01-16 18:07:31 -0300

    Update twitter.py

--- cogs/twitter/twitter.py
+++ cogs/twitter/twitter.py
@@ -2,7 +2,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import config.config
+import config.config as config
 
 #User array
 following = config.TWITTER_FOLLOWING
@@ -85,4 +85,5 @@ class Twitter(commands.Cog, name="Twitter"):
 
 async def setup(bot: commands.Bot):
   await bot.add_cog(Twitter(bot))
+
 
