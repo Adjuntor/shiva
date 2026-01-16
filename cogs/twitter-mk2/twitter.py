942acc7ec034e21a5c6c6ef24f73d731e7ba4c67
Adjuntor <10227133+Adjuntor@users.noreply.github.com>
2026-01-16 18:07:03 -0300

    Update twitter.py

--- cogs/twitter-mk2/twitter.py
+++ cogs/twitter-mk2/twitter.py
@@ -3,7 +3,7 @@ import os
 import os.path
 import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import config
+import config.config
 import time
 import re
 import json
