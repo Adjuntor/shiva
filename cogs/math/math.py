ac03e2dab02f7c3e148f88aea7d0754088a3ae0d
Adjuntor <10227133+Adjuntor@users.noreply.github.com>
2026-01-16 18:10:22 -0300

    Update math.py

--- cogs/math/math.py
+++ cogs/math/math.py
@@ -2,7 +2,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import config
+import config.config as config
 
 #Discord lib
 import discord
@@ -52,4 +52,5 @@ class Math(commands.Cog, name="Math"):
              return await interaction.response.send_message(f'Expression:```{expression}```\nResult:```{error}```')
 
 async def setup(bot: commands.Bot):
+
     await bot.add_cog(Math(bot))
