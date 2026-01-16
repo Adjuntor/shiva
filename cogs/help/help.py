c043b4843bf2e7d97c1882641bc0fbd9f4200f9d
Adjuntor <10227133+Adjuntor@users.noreply.github.com>
2026-01-16 18:10:51 -0300

    Update help.py

--- cogs/help/help.py
+++ cogs/help/help.py
@@ -2,7 +2,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import config
+import config.config as config
 
 #Discord lib
 import discord
@@ -24,4 +24,5 @@ class Help(commands.Cog, name="Help"):
         return
 
 async def setup(bot: commands.Bot):
+
   await bot.add_cog(Help(bot))
