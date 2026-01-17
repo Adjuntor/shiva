e33a9e94d3ef838eb48261d104bfaf078107b858
Adjuntor <10227133+Adjuntor@users.noreply.github.com>
2026-01-16 23:17:39 -0300

    Update rss.py

--- cogs/rss/rss.py
+++ cogs/rss/rss.py
@@ -18,32 +18,30 @@ c = connection.cursor()
 c.execute("""CREATE TABLE IF NOT EXISTS articles (title TEXT, link TEXT)""")
 connection.commit()
 
-def record_article_in_db(article):
+async def record_article_in_db(article):
 	c.execute("""INSERT INTO articles (title, link) VALUES (?, ?)""", (article.title, article.link))
 	connection.commit()
 
-def article_in_db(entry):
+async def article_in_db(entry):
 	c.execute("""SELECT link FROM articles WHERE link=?""", (entry.link,))
 	if c.fetchone() is None:
 		return False
 	else:
 		return True
 
-
-def get_new_articles():
+async def get_new_articles():
     new_articles = []
     for rss_feed in config.RSS_FEEDS:
         entries = feedparser.parse(rss_feed["url"]).entries
         for entry in entries:
-            if not article_in_db(entry):
+            if not await article_in_db(entry):
                 posttime = entry.published.split('GMT')
                 pub_date = datetime.strptime(posttime[0].strip(), "%a, %d %b %Y %H:%M:%S").replace(tzinfo=timezone.utc)
                 if datetime.now(timezone.utc) - pub_date <= timedelta(days=config.RSS_LAST_ARTICLE_RANGE):
                     new_articles.append({"article": entry, "channel": rss_feed["channel"], "feedTitle": feedparser.parse(rss_feed["url"]).feed.title})
     return new_articles
 
-
-def format_to_message(article):
+async def format_to_message(article):
     article_link = article["article"].link
     if "https://x.com" in article_link:
         article_link = article_link.replace("https://x.com","https://fxtwitter.com")
@@ -54,9 +52,7 @@ def format_to_message(article):
     if "http://twitter.com" in article_link:
         article_link = article_link.replace("http://twitter.com","https://fxtwitter.com")
     message = f"{article_link}"
-
     return message
-
 
 class RSS(commands.Cog, name="RSS"):
     """RSS cog"""
@@ -68,13 +64,13 @@ class RSS(commands.Cog, name="RSS"):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @tasks.loop(minutes = config.RSS_UPDATE_INTERVAL)
+    @tasks.loop(seconds = config.RSS_UPDATE_INTERVAL)
     async def rss(self):
-        new_articles = get_new_articles()
+        new_articles = await get_new_articles()
         for article in new_articles:
-            message = format_to_message(article)
+            message = await format_to_message(article)
             await self.bot.get_channel(int(article["channel"])).send(message)
-            record_article_in_db(article["article"])
+            await record_article_in_db(article["article"])
  
 async def setup(bot: commands.Bot):
     await bot.add_cog(RSS(bot))
