b1551dcdeb1a3b5aed7e244e106f01db0ea2ff3e
Adjuntor <10227133+Adjuntor@users.noreply.github.com>
2026-01-17 16:12:39 -0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -6,35 +6,8 @@ EMBEDCOLOR=0x408020
 #####################################################################################
 
 ################################----COGS TO LOAD----#################################
-ACTIVECOGS = ["activity.py", "cleanup.py", "help.py", "color.py", "hclasses.py", "hwelcome.py", "math.py", "moderation.py", "rss.py", "say.py", "sync.py", "twitter.py", "twitter-mk2.py"]
+ACTIVECOGS = ["activity.py", "cleanup.py", "help.py", "color.py", "hclasses.py", "hwelcome.py", "math.py", "moderation.py", "say.py", "sync.py"]
 #####################################################################################
-
-###################################----TWITTER----###################################
-#Set the array as [twitter_user_id, discord_channel_id]
-#twitter_user_id: the id of the person you want to follow, ex: 44196397
-#discord_channel_id: the id of the discord channel you want to post all tweets
-TWITTER_FOLLOWING = [
-    [44196397,4059123452180252830],  # Elon Musk
-    [11348282,9959194743629204856],  # Nasa
-    [11348282,5493859438512378459],  # Nasa again but in another channel
-    [1072404907230060544, 8937832461213380938] # Genshin Impact
-]
-
-# URL(s) to scrape
-FOLLOWING = ["https://twitter.com/NIKKE_en", "https://twitter.com/honkaiimpact3rd"]
-
-# Path to chrome driver to be used by selenium
-CHROME_DRIVER = ""
-
-# Path to storage/cache
-PATH_TO_STORAGE = ""
-
-# Time to wait for page to load
-TIME_TO_WAIT_IN_SECONDS = 8
-
-#Twitter tokens, need ELEVATED priviledge
-TWITTER_BEARER=r"AAAAAAAAAAAAAAAAAAAAAE1234567890LKnfikaujFJANSKJfbakhsbfakhsnfoF#*(NFOQOGFN"
-#######################################################################################
 
 ##################################----MODERATION----###################################
 #Channel to log deleted or edited messages
@@ -70,7 +43,6 @@ PLAY_STAT = ["on the web", "videogames"]# Playing ...
 WATCH_STAT = ["over the web"]# Watching ...
 LISTEN_STAT = ["the web"]#Listening to ...
 ########################################################################################
-
 
 ##################################----NAME COLORS----###################################
 #Set the guild id and role id of each color
@@ -109,23 +81,3 @@ ROLE_COLOR_PINK=[
     [1234567890,987654321],
 ]
 ########################################################################################
-
-####################################----RSS----####################################
-# Update interval for how often the bot is supposed to check if a new entry in the RSS feed exists (in Seconds)
-RSS_UPDATE_INTERVAL = 60
-
-# How far a new entry in the RSS feed can can be published in the past before being ignored (in Days)
-RSS_LAST_ARTICLE_RANGE = 5
-
-# Add the RSS feeds here. Each object consists of the RSS feed URL and the channel ID you want to post on
-RSS_FEEDS = [
-    {
-        "url": "http://rss.feed",
-        "channel": "1460912367289625245"
-    },
-    {
-        "url": "http://website.feed",
-        "channel": "14123673739089625245"
-    }
-]
-#######################################################################################
