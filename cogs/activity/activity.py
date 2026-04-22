89f6776b1fb566a3a639be00a66732580d9fd0a0
Adjuntor <10227133+Adjuntor@users.noreply.github.com>
2026-04-22 16:05:11 -0300

    Update activity.py

--- cogs/activity/activity.py
+++ cogs/activity/activity.py
@@ -4,6 +4,7 @@ import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import config.config as config
 
+
 #Discord lib
 import discord
 from discord.ext import commands, tasks
